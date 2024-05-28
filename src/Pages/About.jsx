2a2f316fd99b728a96a14a969122fdd38a023b31
kunal yadav <kunalyadav1601@gmail.com>
2024-05-28 19:43:50 +0530

    praveen name removed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         Hello, I'm{" "}
         <span className='blue-gradient_text font-semibold drop-shadow'>
           {" "}
-          Praveen
+          Kunal
         </span>{" "}
         👋 
       </h1>
